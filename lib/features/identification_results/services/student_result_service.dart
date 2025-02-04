ae29d50fa8d132feae0b8884201756763e069260
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-02-04 13:43:28 +0800

    feat: Add student result service

---
+++ lib/features/identification_results/services/student_result_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class StudentIdentificationResultService {
+  final String baseUrl;
+
+  StudentIdentificationResultService() : baseUrl = dotenv.get('API_URL');
+
+  Future<bool> updateQuestionResult(
+      String questionResultId, bool isCorrect) async {
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/identification-results/question/$questionResultId'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'isCorrect': isCorrect}),
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to update question result');
+      }
+
+      return true;
+    } catch (e) {
+      print("Error updating question result: $e");
+      throw Exception('Error updating question result: $e');
+    }
+  }
+}
